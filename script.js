83589c44bb68464ea7a4ebfcfc18210c0969cb42
Wanessa-Guedes <wanessa.o.guedes@gmail.com>
2022-02-02 18:24:42 +0000

    Girando as cartas e colocando foto de verso

--- script.js
+++ script.js
@@ -12,7 +12,14 @@ let qteCartasJogo = "";
 let arrayCartas = [];
 
 for(let i = 0; i < qteCartas; i++){
-    qteCartasJogo += '<div id="carta'+i+'" class="corpoCarta" onclick="virarCarta()"></div>';
+    /* qteCartasJogo += '<div id="carta'+i+'" class="corpoCarta" onclick="rodarCarta()"></div>'; */
+    qteCartasJogo += `<div class="card" onclick="testeClicar(this)">
+                            <div class="front-face face">
+                            <img src='imagens/front 1.png' alt='Frente da carta - papagaio'>
+                            </div>
+                            <div class="back-face face">
+                            </div>
+                    </div>`;
     arrayCartas += i;
 }
 
@@ -22,23 +29,10 @@ for(let i = 0; i < qteCartas; i++){
 
 // querySelector eu consigo selecionar os elementos que tem determinada classe? As imagens vão estar tudo dentro de main
 
-let elemento = document.querySelector("main");
-
+let elemento = document.querySelector(".teste");
 // resgatar o que esteja dentro do elemento ou atribuir algo a ele? Tipo colocar a carta dentro da div com essa classe?
 
 elemento.innerHTML = qteCartasJogo; 
-
-// Colocar imagem na frente - Ver as divs dentro do main 
-let insideElemento = elemento.children;
-
-// Criar imagem no documento
-let img_frente = "<img src='imagens/front 1.png' alt='Frente da carta - papagaio'>";
-
-// insideElemento.appendChild(img_frente) 
-// Agora eu tenho os elementos dentro do main que acrescentei e tenho que percorrer eles para acrescentar as imagens 
-for(let carta of insideElemento){
-    carta.innerHTML = img_frente;
-}
 
 // Jogo -- Passos
 // 1 - clicar na carta, 2 - carta virar, 3- aparecer a imagem da carta virada 
@@ -59,16 +53,28 @@ for(let i = 0; i < qteCartas; i+=2){
 }
 
 indiceCartas.sort(comparador); // Após esta linha, a minhaArray estará embaralhada
-
-
 // Esta função pode ficar separada do código acima, onde você preferir
 function comparador() { 
 	return Math.random() - 0.5; 
 }
 
-function jogo(){
+// Clicando na carta e fazendo girar
 
-    // virar a carta
+function testeClicar(card){
+    let front = card.querySelector(".front-face"); 
+    let back = card.querySelector(".back-face"); 
 
-    
+    front.classList.toggle('flip-front');
+    back.classList.toggle('flip-back');
 }
+
+// Agora colocar a imagem do verso de acordo com o indice sorteado
+let imgBack = [];
+let backFace = document.querySelectorAll(".back-face");
+
+for(let i = 0; i < qteCartas; i++){
+    imgBack[i] = '<img src=imagens/back' + indiceCartas[i] + '.png>'
+    backFace[i].innerHTML = imgBack[i];
+}
+
+
