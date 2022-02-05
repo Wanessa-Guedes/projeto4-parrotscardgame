4eb79a8eded06309fc0a0bf49375082a2aabae39
Wanessa-Guedes <wanessa.o.guedes@gmail.com>
2022-02-05 22:15:10 +0000

    Versão com comentários e arrumado erro de poder clicar na carta enquanto as outras ainda não tinham desvirado

--- script.js
+++ script.js
@@ -1,50 +1,29 @@
-// Repetições do início do Jogo
+// Iniciando o jogo -- recebendo quantidade de cartas 
 
 let qteCartas = 0;
 
 while (qteCartas < 4 || qteCartas > 14 || qteCartas % 2 != 0) {
-    qteCartas = parseInt(prompt("Com quantas cartas quer jogar? Escolha entre 4 e 14!"));
+    qteCartas = parseInt(prompt("Com quantas cartas quer jogar? Escolha um número par entre 4 e 14!"));
 }
 
-// Criando a qte de cartas a serem visualizadas com a escolha de inicialização do jogador
+// Criando o html de acordo com a qte de cartas escolhidas na inicialização pelo jogador
 
 let qteCartasJogo = "";
-let arrayCartas = [];
 
 for (let i = 0; i < qteCartas; i++) {
-    /* qteCartasJogo += '<div id="carta'+i+'" class="corpoCarta" onclick="rodarCarta()"></div>'; */
-    qteCartasJogo += `<div id =carta${i} class="card" onclick="testeClicar(this)" data-identifier="card">
+    qteCartasJogo += `<div id =carta${i} class="card" onclick="clicar(this)" data-identifier="card">
                             <div class="front-face face" data-identifier="front-face">
                             <img src='imagens/front 1.png' alt='Frente da carta - papagaio'>
                             </div>
                             <div class="back-face face" data-identifier="back-face">
                             </div>
                     </div>`;
-    arrayCartas += i;
 }
 
-// Usar id no console log aparece.. <div id="carta0" class="corpoCarta"> </div><div id="carta1" class="corpoCarta"> </div><div id="carta2" class="corpoCarta"> </div><div id="carta3" class="corpoCarta"> </div>
-
-// Agora o qteCartasJogo parece estar se comportando bem, mas como colocar as imagens? Coloquei direto a da frente, será que pode?
-
-// querySelector eu consigo selecionar os elementos que tem determinada classe? As imagens vão estar tudo dentro de main
-
-let elemento = document.querySelector(".teste");
-// resgatar o que esteja dentro do elemento ou atribuir algo a ele? Tipo colocar a carta dentro da div com essa classe?
-
+let elemento = document.querySelector(".kard");
 elemento.innerHTML = qteCartasJogo;
 
-// Jogo -- Passos
-// 1 - clicar na carta, 2 - carta virar, 3- aparecer a imagem da carta virada 
-//4 - programa precisa saber qual carta é (algum tipo de indice?)
-// 5 - armazenar o indice da carta virada para comparar com os das outras cartas
-//  6 -  Quando virar e escolheu duas, se acertou continuam viradas e fazer o programa entender que faltam menos cartas
-// 6 - se não acertou, as cartas desviram e mantêm-se na mesma posição
-// 7 - o jogo é finalizado quando todas as cartas são desviradas e permanecem assim
-
-// Fazer o sorteio das cartas
-//push -- sempre adiciona o item ao final da array sem a necessidade de você saber o último índice:
-// indices foram adicionados as cartas
+// indices adicionados as cartas - Usando +2 para adicionar de 2 em 2 e auxiliar nos pares
 let indiceCartas = [];
 let verificaCartas = [];
 
@@ -52,12 +31,12 @@ for (let i = 0; i < qteCartas; i += 2) {
     indiceCartas.push(i);
     indiceCartas.push(i);
 
-    // Fazer um push de true para as cartas
+    // Fazer um push de true para as cartas - auxílio para virar e desvirar com o clique
     verificaCartas.push(true);
     verificaCartas.push(true);
 }
 
-console.log(indiceCartas);
+// Fazer o sorteio das cartas com os indices selecionados
 
 indiceCartas.sort(comparador); // Após esta linha, a minhaArray estará embaralhada
 
@@ -65,8 +44,9 @@ function comparador() {
     return Math.random() - 0.5;
 }
 
-// Clicando na carta e fazendo girar
-let container = [null, null];
+// Iniciando a lógica do jogo
+
+let container = [null,null];
 let armazenarPares = 0;
 let armazenaCartas = [null, null];
 let armazenaId = [null, null];
@@ -74,73 +54,90 @@ let infoPar = false;
 let contJogadas = 0;
 let contador = document.querySelector(".NoJogadas");
 
-function testeClicar(card) {
+// Função que inicia a lógica do jogo ao clicar na carta
+function clicar(card) {
+
     // Assim tenho o índice da carta clicada
-    let numCard = parseFloat(card.id.replace('carta', '')); // Número das divs que contem as cartas
-    contador.innerHTML = `Quantidade de jogadas: ${contJogadas + 1}`;
+    let numCard = parseFloat(card.id.replace('carta', '')); 
+    
+    // Comparar se a carta já foi clicada e se ela já fez par... Se nenhuma das duas condições for atendida
+    // ai pode virar a carta
+
     if (verificaCartas[numCard] == true && infoPar == false) {
 
         virarCarta(card);
-        // Depois que virou a carta coloca falso pra não poder desvirar -- ai teria que colocar um ou no if pq 
-        // se não for par ai tem que desvirar
+
+        // Depois que virou a carta coloca falso pra não poder desvirar  
         verificaCartas[numCard] = false;
 
-        // Identificar e armazenar qual carta está lá
+        // Identificar se a carta 1 (primeira clicada) já foi alocada na posição 0
         if (container[1] == null && container[0] != null) {
+            // se sim, armazena o número da carta e seu identificador
             armazenaCartas[1] = numCard;
             armazenaId[1] = card.id;
+            // Se não for a mesma carta que foi clicada -- verificada pelo id
+            // armazena a carta 2 no container e conta como mais uma jogada
+            // se for a mesma carta o container 1 volta a ser nulo
             if (armazenaCartas[1] != armazenaCartas[0]) {
                 container[1] = indiceCartas[numCard];
                 contJogadas++;
+                contador.innerHTML = `Quantidade de jogadas: ${contJogadas}`;
             } else {
                 container[1] = null;
+                contJogadas--;
             }
         }
+        // Se o container 0 não tiver recebido nenhuma carta, ele recebe ela e dentro do if
+        // armazena o id e o número da carta e conta mais jogada
         if (container[0] == null) {
             container[0] = indiceCartas[numCard];
             armazenaCartas[0] = numCard;
             armazenaId[0] = card.id;
             contJogadas++;
+            contador.innerHTML = `Quantidade de jogadas: ${contJogadas}`;
         }
 
-        // Comparar o que tem dentro do container
+        // Comparar se as duas posições do container receberam uma carta
         if (container[0] != null && container[1] != null) {
+            // Comparar de são pares e armazena como pares
             if (container[0] == container[1]) {
                 armazenarPares += 2;
-                console.log('Deu certo');
-                console.log(armazenarPares);
+            //    Se a quantidade de pares for menor que a quantidade de cartas -- Ainda tem jogo
                 if (armazenarPares < qteCartas) {
-                    console.log('Tem jogo ainda...');
+                //    Armazenou os pares e esvaziou os containers para receber mais cartas
                     container = [null, null];
-                    infoPar == true;
-                    /* armazenaId = [null, null]; */
+                    // Se o valor de pares é igual ao número de cartas final de jogo
+                    // A variável booleana que informa se são pares e seta a virada da carta com o clique
+                    // é ativada, assim mesmo se clicar nas cartas não vira mais
                 } else if (armazenarPares == qteCartas) {
                     setTimeout(fimDeJogo, 500);
-                    infoPar == true;
+                    infoPar = true;
                 }
             } else {
-                infoPar = false;
+                // se não forem par o a variável que informa se é par é ativada para impedir que 
+                // possa clicar em outras cartas e elas virarem quando ainda as cartas anteriores ainda 
+                // não foram desviradas... Na função desvirarCarta essa variável volta a false para poder
+                // virar as cartas novamente
+                infoPar = true;
+                // se não for par as cartas podem ser viradas novamente
                 for (let i = 0; i < container.length; i++) {
                     verificaCartas[armazenaCartas[i]] = true;
                 }
+
+                // função de desvirar cartas com o tempo de 1 segundo
                 setTimeout(function () {
                     desvirarCarta(armazenaId);
                 }, 1000);
-                /* armazenaId = [null, null]; */
-                container = [null, null];
-                console.log('Tente de novo =/');
-            }
-        }
 
-        /*         
-                console.log(container);
-                console.log(virarCarta);
-                console.log(infoPar);
-                console.log(verificaCartas); */
+                // containers esvaziados                
+                container = [null, null];
+            }
+
+        }
     }
 }
 
-
+// Função para virar a carta
 function virarCarta(card) {
     let front = card.querySelector(".front-face");
     let back = card.querySelector(".back-face");
@@ -149,6 +146,7 @@ function virarCarta(card) {
     back.classList.toggle('flip-back');
 }
 
+// Função para desvirar as cartas e permitir que elas sejam clicáveis novamente
 function desvirarCarta(armazenaId) {
 
     let carta0 = document.getElementById(armazenaId[0]);
@@ -159,49 +157,17 @@ function desvirarCarta(armazenaId) {
     carta1.children[0].classList.remove('flip-front');
     carta1.children[1].classList.remove('flip-back');
 
-    console.log(carta0);
-    console.log(carta1);
-    /*     let front = card.querySelector(".front-face");
-        let back = card.querySelector(".back-face");
-    
-        front.classList.toggle('flip-front');
-        back.classList.toggle('flip-back'); */
+    infoPar = false;
 }
 
+// Função para alertar que o jogo acabou e informar o número de jogadas realizadas
 function fimDeJogo() {
     alert(`Fim de jogo!! Parabéns! Você ganhou com ${contJogadas} jogadas!
             
 Para jogar novamente, por favor, recarregue a página!`);
 }
-/* let verificador = [null, null];
-let auxVerificador = "";
-// Verificar se a carta já foi viradas
-function verificaCarta(numCard){  
 
-    if(verificador[0] != null){
-        verificador[1] = numCard;
-    }else {
-        verificador[0] = numCard;
-    }
-
-    if(verificador[0] != null && verificador[1] != null){
-        if(verificador[0] == verificador[1]){
-            auxVerificador = false;
-            console.log(auxVerificador);
-            console.log(verificador);
-            verificador = [null, null]
-        } else {
-            auxVerificador = true;
-            console.log(auxVerificador);
-            console.log(verificador);
-            verificador = [null, null]
-        }
-    }
-
-    return auxVerificador;
-} */
-
-// Agora colocar a imagem do verso de acordo com o indice sorteado
+// Colocando a imagem do verso de acordo com o indice sorteado
 let imgBack = [];
 let backFace = document.querySelectorAll(".back-face");
 
@@ -209,11 +175,3 @@ for (let i = 0; i < qteCartas; i++) {
     imgBack[i] = '<img src=imagens/back' + indiceCartas[i] + '.gif>'
     backFace[i].innerHTML = imgBack[i];
 }
-
-console.log(indiceCartas);
-
-// desvirar as duas cartas depois que a segunda foi aberta se os índices não forem iguais
-// Clicou na carta... Viu quem virou, pegou o id dela... Com o id consigo saber qual carta é a virada
-
-
-
